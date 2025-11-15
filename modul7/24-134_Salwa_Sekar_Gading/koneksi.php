447475d109fc233bfa3738553942228ad43ebc5e
24-134 Salwa Sekar Gading <salwasekar00@gmail.com>
2025-11-16 00:11:15 +0700

    Add files via upload

---
+++ modul7/24-134_Salwa_Sekar_Gading/koneksi.php
@@ -0,0 +1,16 @@
+<?php 
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "modul7";
+
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+
+// if ($conn){
+//     echo "koneksi berhasil";
+// }else{
+//     echo "eror: " . mysqli_connect_error();
+// }
+
+?>
